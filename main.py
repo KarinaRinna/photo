2a283b6d2b6e9f0b38d16b031b0a3aaf4df6688f
Mandarin <zurual@mail.ru>
2023-03-20 23:06:13 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import easyocr
+
+
+def text_recognition(file_path):
+    reader = easyocr.Reader(["ru"])
+    result = reader.readtext(file_path)
+
+    return result
+
+
+def main():
+    file_path = input("Enter a file path: ")
+    text_recognition(file_path="file_path")
+
+
+if __name__ == "__main__":
+    main()
